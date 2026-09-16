2d91668750f67927832b5f3b6518910c1b93ce6a
Robert Maier <maier.robert@googlemail.com>
2026-09-16 21:05:56 +0200

    upgrade to OpenCV 4 and newer

--- lsd_slam_core/src/util/Undistorter.cpp
+++ lsd_slam_core/src/util/Undistorter.cpp
@@ -24,7 +24,7 @@
 #include <fstream>
 
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/calib3d.hpp>
 #include <Eigen/Core>
 #include "util/settings.h"
 
